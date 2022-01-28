7c5fe52346e6db99eaf309f109e7962261662569
gurselturkeri <68682737+gurselturkeri@users.noreply.github.com>
2022-01-28 14:38:10 +0300

    Update yeni_efekt.py

--- yeni_efekt.py
+++ yeni_efekt.py
@@ -10,17 +10,17 @@ fotolar = os.scandir("/home/gursel/Desktop/pygurs")    # fotoğraflarımızın o
 
     
     
-num_files = len([f for f in os.listdir("/home/gursel/Desktop/pygurs")if os.path.isfile(os.path.join("/home/gursel/Desktop/pygurs", f))])
+num_files = len([f for f in os.listdir("/home/gursel/Desktop/pygurs")if os.path.isfile(os.path.join("/home/gursel/Desktop/pygurs", f))])  # toplam dosya sayısı
 eklenilen_foto = 0    
 
 while True:
     
     foto_sayi = input("foto sayisini gir: ")            
     image = cv2.imread("/home/gursel/Desktop/pygurs/{}.jpg".format(foto_sayi)) # fotoğrafları tek tek for döngüsü sayesinde okuttuk
-   # image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    # cv2.imshow("img{}".format(foto_sayi),image)
+   
     eklenilen_foto += 1
 
+    ### Efektin olduğu kod satırları
     transform = A.Compose(
     [A.RandomRain(brightness_coefficient=0.9, drop_width=1, blur_value=5, p=1)],
     )
@@ -30,9 +30,9 @@ while True:
 
 
     
-    cv2.imwrite("/home/gursel/Desktop/efektli/efektli_{}.jpg".format(foto_sayi),transformed['image'])
+    cv2.imwrite("/home/gursel/Desktop/efektli/efektli_{}.jpg".format(foto_sayi),transformed['image'])  # efekt uyguladığın fotoğrafı başka yere kaydet
     
-    if eklenilen_foto == num_files:
+    if eklenilen_foto == num_files:     # eğer klasördeki fotoğraf sayısı kadar yaptıysan program biter
         break
     
 
