1d0466c7f2960330ab75b5673316cd52f91051b5
gurselturkeri <68682737+gurselturkeri@users.noreply.github.com>
2022-01-28 14:36:02 +0300

    Update ad_degistirme_new.py

--- ad_degistirme_new.py
+++ ad_degistirme_new.py
@@ -1,7 +1,7 @@
 import os
-path = '/home/gursel/Desktop/2022/sola_donulmez'
+path = '/home/gursel/Desktop/2022/sola_donulmez'        # dosya yolunu ekle
 files = os.listdir(path)
 
 
-for index, file in enumerate(files):
-    os.rename(os.path.join(path, file), os.path.join(path, ''.join([str(index), '.jpg'])))
+for index, file in enumerate(files):       # klasördeki dosyların sayısal değerlerleri kadar for döngüsünü çalıştır
+    os.rename(os.path.join(path, file), os.path.join(path, ''.join([str(index), '.jpg'])))         # dosyaların adını 0dan 1e kadar değiştir 
